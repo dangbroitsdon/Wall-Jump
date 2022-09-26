36c882baf06cc0301bc540e96bff3e92e9ae3e92
dangbroitsdon <hoodzabyss@gmail.com>
2022-09-26 07:28:20 -0400

    v1.5.3.5
    more enchantment fixes sorry gamers

--- common/src/main/java/genandnic/walljump/mixin/MixinPlayerList.java
+++ common/src/main/java/genandnic/walljump/mixin/MixinPlayerList.java
@@ -1,7 +1,6 @@
 package genandnic.walljump.mixin;
 
 import genandnic.walljump.config.WallJumpConfig;
-import genandnic.walljump.registry.WallJumpReceivers;
 import io.netty.buffer.Unpooled;
 import me.shedaniel.architectury.networking.NetworkManager;
 import net.minecraft.network.Connection;
@@ -22,7 +21,6 @@ public class MixinPlayerList {
         System.out.println("[Wall-Jump! UNOFFICIAL] Syncing Server Config");
         FriendlyByteBuf packet = new FriendlyByteBuf(Unpooled.buffer());
         packet.writeBoolean(WallJumpConfig.getConfigEntries().enableWallJump);
-        packet.writeBoolean(WallJumpConfig.getConfigEntries().enableWallJumpEnchantment);
 //        if(!WallJumpConfig.getConfigEntries().blockBlacklist.isEmpty()) {
 //            for (String blocks : WallJumpConfig.getConfigEntries().blockBlacklist) {
 //                packet.writeUtf(blocks);
@@ -36,14 +34,12 @@ public class MixinPlayerList {
         packet.writeInt(WallJumpConfig.getConfigEntries().delayWallClingSlide);
         packet.writeDouble(WallJumpConfig.getConfigEntries().exhaustionWallJump);
         packet.writeBoolean(WallJumpConfig.getConfigEntries().enableDoubleJump);
-        packet.writeBoolean(WallJumpConfig.getConfigEntries().enableDoubleJumpEnchantment);
         packet.writeInt(WallJumpConfig.getConfigEntries().countDoubleJump);
         packet.writeDouble(WallJumpConfig.getConfigEntries().exhaustionDoubleJump);
         packet.writeBoolean(WallJumpConfig.getConfigEntries().playFallingSound);
         packet.writeDouble(WallJumpConfig.getConfigEntries().minFallDistance);
         packet.writeDouble(WallJumpConfig.getConfigEntries().elytraSpeedBoost);
         packet.writeDouble(WallJumpConfig.getConfigEntries().sprintSpeedBoost);
-        packet.writeBoolean(WallJumpConfig.getConfigEntries().enableSpeedBoostEnchantment);
         packet.writeBoolean(WallJumpConfig.getConfigEntries().enableStepAssist);
         NetworkManager.sendToPlayer(serverPlayer, SERVER_CONFIG_PACKET_ID, packet);
         System.out.println("[Wall-Jump! UNOFFICIAL] Synced Server Config");
--- common/src/main/java/genandnic/walljump/registry/WallJumpReceivers.java
+++ common/src/main/java/genandnic/walljump/registry/WallJumpReceivers.java
@@ -4,7 +4,6 @@ import genandnic.walljump.config.WallJumpConfig;
 import io.netty.buffer.Unpooled;
 import me.shedaniel.architectury.networking.NetworkManager;
 import net.minecraft.network.FriendlyByteBuf;
-import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.entity.player.Player;
 
 import static genandnic.walljump.WallJump.*;
@@ -38,7 +37,6 @@ public class WallJumpReceivers {
 
         NetworkManager.registerReceiver(NetworkManager.s2c(), SERVER_CONFIG_PACKET_ID, (buf, context) -> {
             WallJumpConfig.getConfigEntries().enableWallJump = buf.readBoolean();
-            WallJumpConfig.getConfigEntries().enableWallJumpEnchantment = buf.readBoolean();
 //            TODO: figure this out in v1.5.4: the rewrite
 //            if(!WallJumpConfig.getConfigEntries().blockBlacklist.isEmpty()) {
 //                List<String> ls1 = new ArrayList<>();
@@ -55,14 +53,12 @@ public class WallJumpReceivers {
             WallJumpConfig.getConfigEntries().delayWallClingSlide = buf.readInt();
             WallJumpConfig.getConfigEntries().exhaustionWallJump = buf.readDouble();
             WallJumpConfig.getConfigEntries().enableDoubleJump = buf.readBoolean();
-            WallJumpConfig.getConfigEntries().enableDoubleJumpEnchantment = buf.readBoolean();
             WallJumpConfig.getConfigEntries().countDoubleJump = buf.readInt();
             WallJumpConfig.getConfigEntries().exhaustionDoubleJump = buf.readDouble();
             WallJumpConfig.getConfigEntries().playFallingSound = buf.readBoolean();
             WallJumpConfig.getConfigEntries().minFallDistance = buf.readDouble();
             WallJumpConfig.getConfigEntries().elytraSpeedBoost = buf.readDouble();
             WallJumpConfig.getConfigEntries().sprintSpeedBoost = buf.readDouble();
-            WallJumpConfig.getConfigEntries().enableSpeedBoostEnchantment = buf.readBoolean();
             WallJumpConfig.getConfigEntries().enableStepAssist = buf.readBoolean();
             System.out.println("[Wall-Jump! UNOFFICIAL] Server Config has been received and synced on Client!");
             serverConfigSynced = true;
